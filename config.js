f2acf4ffee62c8b4505efe8d599418e20962ec58
Agastore <mohanggakurniawanf12@gmail.com>
2024-08-24 06:17:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js
+const numberAllowed = ['6283890773329','6283861585742']; // Nomor yang diizinkan untuk chat ke bot, tambahkan kalau diperlukan
+
+global.prefix = ['.', '#']; // Daftar prefix
+
+global.jeda = 5000; // 5 detik jeda pengiriman untuk pushkontak atau broadcast
+
+global.name_script = 'Script Jpm';
+
+global.version = '1.1';
+
+
+module.exports = { numberAllowed};
